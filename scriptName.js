c6c134fa3394ec63f86d29c5f48f9096ba25c385
Bryant Meskill <bryant.meskill@gmail.com>
2021-11-25 11:08:58 -0600

    Basic functions in place.

---
+++ scriptName.js
@@ -0,0 +1,59 @@
+const computerSelection = computerPlay();
+let playerSelection = prompt(
+  "Please enter rock, paper, or scissors: "
+).toLowerCase();
+function computerPlay() {
+  x = "rock";
+  y = "paper";
+  z = "scissors";
+  rng = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+  if (rng === 1) {
+    return x;
+  } else if (rng === 2) {
+    return y;
+  } else {
+    return z;
+  }
+}
+
+let d = 0;
+let j = 0;
+let k = 0;
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    console.log(`Draw, you both picked ${playerSelection}`);
+    d++;
+  } else if (playerSelection === "paper" && computerSelection === x) {
+    console.log(
+      `You win this round, ${playerSelection} beats ${computerSelection}!`
+    );
+    j++;
+  } else if (playerSelection === "scissors" && computerSelection === y) {
+    console.log(
+      `You win this round, ${playerSelection} beats ${computerSelection}!`
+    );
+    j++;
+  } else if (playerSelection === "rock" && computerSelection === z) {
+    console.log(
+      `You win this round, ${playerSelection} beats ${computerSelection}!`
+    );
+    j++;
+  } else {
+    console.log(
+      `You lose this round, ${computerSelection} beats ${playerSelection}!`
+    );
+    k++;
+  }
+}
+
+playRound(playerSelection, computerSelection);
+
+if (j > k) {
+  console.log(
+    `You won the series, with a final score of ${j} to ${k} with ${d} draw(s).`
+  );
+} else {
+  console.log(
+    `You won the series, with a final score of ${j} to ${k} with ${d} draw(s).`
+  );
+}
