a53d94cf87f13b481456b0408d563c314e5ce43f
Bryant Meskill <bryant.meskill@gmail.com>
2021-12-15 14:58:55 -0600

    Many variable changes. Thanks @DaneMaison

--- scriptName.js
+++ scriptName.js
@@ -1,85 +1,88 @@
 const rockBtn = document.querySelector(".rock");
 const paperBtn = document.querySelector(".paper");
 const scissorsBtn = document.querySelector(".scissors");
-const x = "rock";
-const y = "paper";
-const z = "scissors";
-let d = 0;
-let j = 0;
-let k = 0;
+const ROCK_STR = "rock";
+const PAPER_STR = "paper";
+const SCISSORS_STR = "scissors";
+let drawTally = 0;
+let winTally = 0;
+let lossTally = 0;
 
 function computerPlay() {
   //math.floor returns largest int <= a given number
   //math.random() multiplied by max + min (3 + 1)
   //max being highest possible result
-  rng = Math.floor(Math.random() * 3 + 1);
+  let rng = Math.floor(Math.random() * 3 + 1);
   if (rng === 1) {
-    return x;
+    return ROCK_STR;
   } else if (rng === 2) {
-    return y;
+    return PAPER_STR;
   } else {
-    return z;
+    return SCISSORS_STR;
   }
 }
 
 rockBtn.addEventListener("click", () => {
-  let computerSelection = computerPlay();
+  const computerSelection = computerPlay();
   playRound("rock", computerSelection);
 });
 
 paperBtn.addEventListener("click", () => {
-  let computerSelection = computerPlay();
+  const computerSelection = computerPlay();
   playRound("paper", computerSelection);
 });
 
 scissorsBtn.addEventListener("click", () => {
-  let computerSelection = computerPlay();
+  const computerSelection = computerPlay();
   playRound("scissors", computerSelection);
 });
 
 const resultsBox = document.querySelector(".results");
 const roundWinner = document.createElement("div");
 const tally = document.createElement("div");
-tally.classList.add("div");
-roundWinner.classList.add("div");
+tally.classList.add("outputTally");
+roundWinner.classList.add("outputTally");
 
 //variable incrementing used for tallying a winner and draw(s).
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     roundWinner.textContent = `Draw, you both picked ${playerSelection}!`;
     resultsBox.appendChild(roundWinner);
-    d++;
-    tally.textContent = `Current score: ${j} wins, ${k} losses, ${d} draw(s).`;
+    drawTally++;
+    tally.textContent = `Current score: ${winTally} wins, ${lossTally} losses, ${drawTally} draw(s).`;
     resultsBox.appendChild(tally);
-  } else if (playerSelection === "paper" && computerSelection === x) {
+  } else if (playerSelection === "paper" && computerSelection === ROCK_STR) {
     roundWinner.textContent = `You win this round, ${playerSelection} beats ${computerSelection}!`;
     resultsBox.appendChild(roundWinner);
-    j++;
-    tally.textContent = `Current score: ${j} wins, ${k} losses, ${d} draw(s).`;
+    winTally++;
+    tally.textContent = `Current score: ${winTally} wins, ${lossTally} losses, ${drawTally} draw(s).`;
     resultsBox.appendChild(tally);
-  } else if (playerSelection === "scissors" && computerSelection === y) {
+  } else if (
+    playerSelection === "scissors" &&
+    computerSelection === PAPER_STR
+  ) {
     roundWinner.textContent = `You win this round, ${playerSelection} beats ${computerSelection}!`;
     resultsBox.appendChild(roundWinner);
-    j++;
-    tally.textContent = `Current score: ${j} wins, ${k} losses, ${d} draw(s).`;
+    winTally++;
+    tally.textContent = `Current score: ${winTally} wins, ${lossTally} losses, ${drawTally} draw(s).`;
     resultsBox.appendChild(tally);
-  } else if (playerSelection === "rock" && computerSelection === z) {
+  } else if (playerSelection === "rock" && computerSelection === SCISSORS_STR) {
     roundWinner.textContent = `You win this round, ${playerSelection} beats ${computerSelection}!`;
     resultsBox.appendChild(roundWinner);
-    j++;
-    tally.textContent = `Current score: ${j} wins, ${k} losses, ${d} draw(s).`;
+    winTally++;
+    tally.textContent = `Current score: ${winTally} wins, ${lossTally} losses, ${drawTally} draw(s).`;
     resultsBox.appendChild(tally);
   } else {
     roundWinner.textContent = `You lose this round, ${computerSelection} beats ${playerSelection}!`;
     resultsBox.appendChild(roundWinner);
-    k++;
-    tally.textContent = `Current score: ${j} wins, ${k} losses, ${d} draw(s).`;
+    lossTally++;
+    tally.textContent = `Current score: ${winTally} wins, ${lossTally} losses, ${drawTally} draw(s).`;
     resultsBox.appendChild(tally);
   }
   //KNOWN ISSUE: You can continue to click an option until the message displayed is changed to a win or loss.
   const verdict = document.createElement("h1");
   verdict.classList.add("verdict");
-  if (j >= 5) {
+  if (winTally >= 5) {
     resultsBox.removeChild(roundWinner);
     resultsBox.removeChild(tally);
     verdict.textContent = "Congratulations, you win! Thanks for playing.";
@@ -87,7 +90,7 @@ function playRound(playerSelection, computerSelection) {
     rockBtn.classList.add("disable");
     paperBtn.classList.add("disable");
     scissorsBtn.classList.add("disable");
-  } else if (k >= 5) {
+  } else if (lossTally >= 5) {
     resultsBox.removeChild(roundWinner);
     resultsBox.removeChild(tally);
     verdict.textContent =
